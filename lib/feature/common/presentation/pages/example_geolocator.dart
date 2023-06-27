fd0cb0ebfed069098f4f6068212a385b21c55384
AdamAbd <adamabd14113@gmail.com>
2023-06-27 16:56:08 +0700

    finish learning geolocator

--- lib/feature/common/presentation/pages/example_geolocator.dart
+++ lib/feature/common/presentation/pages/example_geolocator.dart
@@ -1,182 +1,134 @@
+import 'package:android_intent_plus/android_intent.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class ExampleGeolocator extends StatelessWidget {
+class ExampleGeolocator extends StatefulWidget {
+  const ExampleGeolocator({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Get Location')),
-      body: LocationWidget(),
-    );
-  }
+  State<ExampleGeolocator> createState() => _ExampleGeolocatorState();
 }
 
-class LocationWidget extends StatefulWidget {
-  @override
-  _LocationWidgetState createState() => _LocationWidgetState();
-}
+class _ExampleGeolocatorState extends State<ExampleGeolocator>
+    with WidgetsBindingObserver {
+  List<double>? currentPosition;
 
-class _LocationWidgetState extends State<LocationWidget> {
-  static const String _kLocationServicesDisabledMessage =
-      'Location services are disabled.';
-  static const String _kPermissionDeniedMessage = 'Permission denied.';
-  static const String _kPermissionDeniedForeverMessage =
-      'Permission denied forever.';
-  static const String _kPermissionGrantedMessage = 'Permission granted.';
+  Future<void> _getUserLocation({bool isReturningFromSettings = false}) async {
+    // Check if location service is enabled
+    bool isLocationEnabled = await Geolocator.isLocationServiceEnabled();
 
-  final GeolocatorPlatform _geolocatorPlatform = GeolocatorPlatform.instance;
-  final List<_PositionItem> _positionItems = <_PositionItem>[];
-
-  double vLatitude = 0;
-  double vLongitude = 0;
-
-  void _getUserLocation() async {
-    var position = await GeolocatorPlatform.instance.getCurrentPosition(
-      locationSettings: LocationSettings(
-        accuracy: LocationAccuracy.high,
-        distanceFilter: 0,
-      ),
-    );
-
-    setState(() {
-      vLatitude = position.latitude;
-      vLongitude = position.longitude;
-    });
-  }
-
-  Future<void> _getCurrentPosition() async {
-    final hasPermission = await _handlePermission();
-
-    if (!hasPermission) {
+    if (!isLocationEnabled) {
+      // Show dialog to enable location service
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Location service is disabled"),
+            content: const Text("Please enable location service and try again"),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('Ok'),
+                onPressed: () {
+                  const AndroidIntent intent = AndroidIntent(
+                    action: 'android.settings.LOCATION_SOURCE_SETTINGS',
+                  );
+                  intent.launch();
+                  Navigator.of(context, rootNavigator: true).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
       return;
     }
 
-    final position = await _geolocatorPlatform.getCurrentPosition();
-    _updatePositionList(
-      _PositionItemType.position,
-      position.toString(),
-    );
-  }
+    // Check location permission
+    PermissionStatus permission = await Permission.location.status;
 
-  Future<bool> _handlePermission() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await _geolocatorPlatform.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      _updatePositionList(
-        _PositionItemType.log,
-        _kLocationServicesDisabledMessage,
-      );
-
-      return false;
-    }
-
-    permission = await _geolocatorPlatform.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await _geolocatorPlatform.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        _updatePositionList(
-          _PositionItemType.log,
-          _kPermissionDeniedMessage,
-        );
-
-        return false;
+    if (permission.isDenied || permission.isRestricted) {
+      // Request location permission only if user is not coming back from settings
+      if (!isReturningFromSettings) {
+        permission = await Permission.location.request();
       }
     }
 
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      _updatePositionList(
-        _PositionItemType.log,
-        _kPermissionDeniedForeverMessage,
+    if (permission.isGranted) {
+      var position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.medium,
       );
 
-      return false;
+      setState(() {
+        currentPosition = [position.latitude, position.longitude];
+      });
+    } else {
+      // Show dialog to request location permission
+      if (isReturningFromSettings) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text("Location permission is not granted"),
+              content: const Text(
+                  "Please grant location permission in app settings and try again"),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('Ok'),
+                  onPressed: () {
+                    openAppSettings();
+                    Navigator.of(context, rootNavigator: true).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      }
     }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    _updatePositionList(
-      _PositionItemType.log,
-      _kPermissionGrantedMessage,
-    );
-    return true;
   }
 
-  void _updatePositionList(_PositionItemType type, String displayValue) {
-    _positionItems.add(_PositionItem(type, displayValue));
-    setState(() {});
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _getUserLocation();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _getUserLocation(isReturningFromSettings: true);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 400,
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: _positionItems.length,
-              itemBuilder: (context, index) {
-                final positionItem = _positionItems[index];
-
-                print(positionItem.displayValue);
-
-                if (positionItem.type == _PositionItemType.log) {
-                  return ListTile(
-                    title: Text(
-                      positionItem.displayValue + "Al",
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  );
-                } else {
-                  return Card(
-                    child: ListTile(
-                      title: Text(
-                        positionItem.displayValue + "La",
-                        style: const TextStyle(color: Colors.black),
-                      ),
-                    ),
-                  );
-                }
-              },
+    return Scaffold(
+      appBar: AppBar(title: const Text('Get Location')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Current Position:'),
+            Text(
+              currentPosition == []
+                  ? 'Unknown'
+                  : 'Latitude: ${currentPosition?[0]}, Longitude: ${currentPosition?[1]}',
             ),
-          ),
-          Text('Current Position:'),
-          ElevatedButton(
-            onPressed: _getCurrentPosition,
-            child: Text('Get Location'),
-          ),
-        ],
+            ElevatedButton(
+              onPressed: _getUserLocation,
+              child: const Text('Get Location'),
+            ),
+          ],
+        ),
       ),
     );
   }
-}
-
-enum _PositionItemType {
-  log,
-  position,
-}
-
-class _PositionItem {
-  _PositionItem(this.type, this.displayValue);
-
-  final _PositionItemType type;
-  final String displayValue;
 }
