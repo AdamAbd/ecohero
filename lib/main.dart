ff608cdf17f7ca650622dcbe205274ec96d927e6
AdamAbd <adamabd14113@gmail.com>
2023-06-28 12:50:00 +0700

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:ecohero/feature/feature.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import '../locator.dart' as locator;
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await locator.init();
 
