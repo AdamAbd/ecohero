efe55fabed9032ec0ee10f9d099d21fb86de5b44
AdamAbd <adamabd14113@gmail.com>
2023-07-10 01:12:31 +0700

    refacor profile screen

--- lib/feature/home/presentation/screens/profile_screen.dart
+++ lib/feature/home/presentation/screens/profile_screen.dart
@@ -10,150 +10,173 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: double.infinity,
-      child: BlocBuilder<UserCubit, UserState>(
-        builder: (context, state) {
-          final DocumentReference documentReference = FirebaseFirestore.instance
-              .collection('users')
-              .doc(state.userEntity!.id);
+    FirebaseFirestore db = FirebaseFirestore.instance;
 
-          String vPoin = "";
-          bool isLoading = false;
+    /// User
+    UserEntity? userEntity = context.read<UserCubit>().state.userEntity;
 
-          return FutureBuilder<DocumentSnapshot>(
-            future: documentReference.get(),
-            builder: (BuildContext context,
-                AsyncSnapshot<DocumentSnapshot> snapshot) {
-              if (snapshot.hasError) {
-                vPoin = "Something went wrong";
-                isLoading = false;
-              } else if (snapshot.hasData && !snapshot.data!.exists) {
-                vPoin = "Something went wrong";
-                isLoading = false;
-              } else if (snapshot.connectionState == ConnectionState.done) {
-                Map<String, dynamic> data =
-                    snapshot.data!.data() as Map<String, dynamic>;
-                vPoin = data['poin'].toString();
-                isLoading = false;
-              } else {
-                isLoading = true;
-              }
+    /// Poin
+    String vPoin = "";
+    bool isLoading = false;
 
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+    return SingleChildScrollView(
+      child: FutureBuilder<DocumentSnapshot>(
+        future: db.collection('users').doc(userEntity?.id).get(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.hasError) {
+            vPoin = "Something went wrong";
+            isLoading = false;
+          } else if (snapshot.hasData && !snapshot.data!.exists) {
+            vPoin = "Something went wrong";
+            isLoading = false;
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            Map<String, dynamic> data =
+                snapshot.data!.data() as Map<String, dynamic>;
+            vPoin = data['poin'].toString();
+            isLoading = false;
+          } else {
+            isLoading = true;
+          }
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              Stack(
+                alignment: Alignment.bottomRight,
                 children: [
-                  const SizedBox(height: 20),
-                  Stack(
-                    alignment: Alignment.bottomRight,
-                    children: [
-                      CircleAvatar(
-                        radius: 60,
-                        backgroundImage: NetworkImage(
-                          state.userEntity?.photoURL ?? "",
-                        ),
-                      ),
-                      const CircleAvatar(
-                        radius: 15,
-                        backgroundColor: Colors.amber,
-                        child: Icon(Icons.edit_rounded, size: 18),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 16),
-                  Text(
-                    state.userEntity?.username ?? "",
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
+                  CircleAvatar(
+                    radius: 60,
+                    backgroundImage: NetworkImage(
+                      userEntity?.photoURL ?? "",
                     ),
                   ),
-                  Text(
-                    state.userEntity?.email ?? "",
-                    style: const TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                    ),
+                  const CircleAvatar(
+                    radius: 15,
+                    backgroundColor: Colors.amber,
+                    child: Icon(Icons.edit_rounded, size: 18),
                   ),
-                  const SizedBox(height: 8),
-                  Visibility(
-                    visible: isLoading,
-                    replacement: Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 24,
-                        vertical: 8,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.teal[800],
-                        borderRadius: BorderRadius.circular(28),
-                      ),
-                      child: Text(
-                        "$vPoin Poin",
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    child: const ShimmerLayout(width: 100, height: 36),
-                  ),
-                  const Spacer(),
-                  BlocConsumer<GoogleAuthCubit, GoogleAuthState>(
-                    listener: (context, state) {
-                      if (state is GoogleAuthLoading) {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return const AlertDialog(
-                              title: Text("Loading"),
-                              content: Text("Loading"),
-                            );
-                          },
-                        );
-                      } else {
-                        Navigator.popUntil(
-                          context,
-                          ModalRoute.withName(PagePath.home),
-                        );
-                      }
-
-                      if (state is GoogleAuthSuccess) {
-                        sl<UserCubit>().remove();
-
-                        Navigator.pushNamedAndRemoveUntil(
-                          context,
-                          PagePath.login,
-                          (Route<dynamic> route) => false,
-                        );
-                      } else if (state is GoogleAuthError) {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return const AlertDialog(
-                              title: Text("Failed"),
-                              content: Text("Failed"),
-                            );
-                          },
-                        );
-                      }
-                    },
-                    builder: (context, state) {
-                      return FilledButton(
-                        onPressed: () =>
-                            context.read<GoogleAuthCubit>().googleSignOut(),
-                        child: const Text(
-                          'Logout',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                      );
-                    },
-                  ),
-                  const SizedBox(height: 60),
                 ],
-              );
-            },
+              ),
+              const SizedBox(height: 16),
+              Text(
+                userEntity?.username ?? "",
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Visibility(
+                visible: isLoading,
+                replacement: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 8,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.teal[800],
+                    borderRadius: BorderRadius.circular(28),
+                  ),
+                  child: Text(
+                    "$vPoin Poin",
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                child: const ShimmerLayout(width: 100, height: 36),
+              ),
+              const SizedBox(height: 18),
+              GestureDetector(
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 14),
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const ListTile(
+                    leading: Icon(Icons.shop),
+                    title: Text("Toko"),
+                    subtitle: Text("Tukarkan Poin Anda"),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 14),
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const ListTile(
+                  leading: Icon(Icons.group),
+                  title: Text("Tentang Kami"),
+                  subtitle: Text("Cari tahu lebih banyak"),
+                ),
+              ),
+              const SizedBox(height: 24),
+              BlocConsumer<GoogleAuthCubit, GoogleAuthState>(
+                listener: (context, state) {
+                  if (state is GoogleAuthLoading) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Loading"),
+                          content: Text("Loading"),
+                        );
+                      },
+                    );
+                  } else {
+                    Navigator.popUntil(
+                      context,
+                      ModalRoute.withName(PagePath.home),
+                    );
+                  }
+
+                  if (state is GoogleAuthSuccess) {
+                    Navigator.pushNamedAndRemoveUntil(
+                      context,
+                      PagePath.login,
+                      (Route<dynamic> route) => false,
+                    );
+
+                    sl<UserCubit>().remove();
+                  } else if (state is GoogleAuthError) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Failed"),
+                          content: Text("Failed"),
+                        );
+                      },
+                    );
+                  }
+                },
+                builder: (context, state) {
+                  return GestureDetector(
+                    onTap: () =>
+                        context.read<GoogleAuthCubit>().googleSignOut(),
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 14),
+                      decoration: BoxDecoration(
+                        color: const Color(0xff26B4A1),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: const ListTile(
+                        leading: Icon(Icons.logout),
+                        title: Text("Keluar"),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
           );
         },
       ),
