17edd5af78a4cb52a90e2525c925b65e534aa384
AdamAbd <adamabd14113@gmail.com>
2023-07-09 17:56:21 +0700

    update tap area for challenge item

--- lib/feature/home/presentation/widgets/challenge_item.dart
+++ lib/feature/home/presentation/widgets/challenge_item.dart
@@ -44,176 +44,184 @@ class ChallengeItem extends StatelessWidget {
         Map<String, dynamic> data =
             snapshot.data!.data() as Map<String, dynamic>;
 
-        return Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 14),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Column(
+        return GestureDetector(
+          onTap: tapMessage,
+          child: Container(
+            color: Colors.white,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                isLastIndex
+                    ? const Column(
+                        children: [
+                          Divider(),
+                          SizedBox(height: 14),
+                        ],
+                      )
+                    : const SizedBox(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 14),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Container(
-                        width: 36,
-                        height: 36,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: NetworkImage(data['photoURL'].toString()),
-                          ),
-                          shape: BoxShape.circle,
-                        ),
-                      ),
-                      // Container(
-                      //   width: 1,
-                      //   height: 200,
-                      //   margin: const EdgeInsets.symmetric(vertical: 8),
-                      //   color: Colors.grey,
-                      // ),
-                    ],
-                  ),
-                  const SizedBox(width: 12),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width -
-                        (2 * 14) -
-                        (2 * 18) -
-                        12,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          data['username'].toString(),
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        Text(
-                          desc,
-                          maxLines: 3,
-                          style: const TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.normal,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        Hero(
-                          tag: "imageHeroTransition_$index",
-                          child: Container(
-                            height: 240,
-                            margin: const EdgeInsets.only(top: 4),
+                      Column(
+                        children: [
+                          Container(
+                            width: 36,
+                            height: 36,
                             decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
                               image: DecorationImage(
-                                image: NetworkImage(image),
-                                fit: BoxFit.cover,
+                                image:
+                                    NetworkImage(data['photoURL'].toString()),
+                              ),
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                          // Container(
+                          //   width: 1,
+                          //   height: 200,
+                          //   margin: const EdgeInsets.symmetric(vertical: 8),
+                          //   color: Colors.grey,
+                          // ),
+                        ],
+                      ),
+                      const SizedBox(width: 12),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width -
+                            (2 * 14) -
+                            (2 * 18) -
+                            12,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              data['username'].toString(),
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
                               ),
                             ),
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            IconButton(
-                              onPressed: () {},
-                              splashRadius: 1,
-                              icon: const Icon(Icons.favorite),
+                            Text(
+                              desc,
+                              maxLines: 3,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal,
+                                overflow: TextOverflow.ellipsis,
+                              ),
                             ),
-                            IconButton(
-                              onPressed: tapMessage,
-                              splashRadius: 1,
-                              icon: const Icon(Icons.comment),
+                            Hero(
+                              tag: "imageHeroTransition_$index",
+                              child: Container(
+                                height: 240,
+                                margin: const EdgeInsets.only(top: 4),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  image: DecorationImage(
+                                    image: NetworkImage(image),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
                             ),
-                            IconButton(
-                              onPressed: () {},
-                              splashRadius: 1,
-                              icon: const Icon(Icons.report),
+                            Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  splashRadius: 1,
+                                  icon: const Icon(Icons.favorite),
+                                ),
+                                IconButton(
+                                  onPressed: tapMessage,
+                                  splashRadius: 1,
+                                  icon: const Icon(Icons.comment),
+                                ),
+                                IconButton(
+                                  onPressed: () {},
+                                  splashRadius: 1,
+                                  icon: const Icon(Icons.report),
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            StreamBuilder<QuerySnapshot>(
-              stream: db
-                  .collection("challenge")
-                  .doc(docID)
-                  .collection("followers")
-                  .orderBy("timestamp", descending: true)
-                  .snapshots(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError) {
-                  return const Text("Something went wrong");
-                }
-
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Text("Loading");
-                }
-
-                List<QueryDocumentSnapshot<Object?>> data = snapshot.data!.docs;
-
-                if (data.isEmpty) {
-                  return const SizedBox();
-                }
-
-                return Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 14),
-                      child: Row(
-                        children: [
-                          Stack(
-                            children: List.generate(
-                              min(3, data.length),
-                              (index) {
-                                return Container(
-                                  width: 20,
-                                  height: 20,
-                                  margin: EdgeInsets.only(left: index * 14),
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    border: Border.all(color: Colors.white),
-                                    image: DecorationImage(
-                                      image: NetworkImage(
-                                        data[index]["userPhotoURL"],
-                                      ),
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                          const SizedBox(width: 8),
-                          const Text(
-                            "Tampilkan balasan",
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.normal,
-                              color: Colors.black54,
-                            ),
-                          ),
-                        ],
                       ),
-                    ),
-                    const SizedBox(height: 14),
-                  ],
-                );
-              },
-            ),
-            !isLastIndex
-                ? const Column(
-                    children: [
-                      Divider(),
-                      SizedBox(height: 14),
                     ],
-                  )
-                : const SizedBox()
-          ],
+                  ),
+                ),
+                StreamBuilder<QuerySnapshot>(
+                  stream: db
+                      .collection("challenge")
+                      .doc(docID)
+                      .collection("followers")
+                      .orderBy("timestamp", descending: true)
+                      .snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.hasError) {
+                      return const Text("Something went wrong");
+                    }
+
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Text("Loading");
+                    }
+
+                    List<QueryDocumentSnapshot<Object?>> data =
+                        snapshot.data!.docs;
+
+                    if (data.isEmpty) {
+                      return const SizedBox();
+                    }
+
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 14),
+                          child: Row(
+                            children: [
+                              Stack(
+                                children: List.generate(
+                                  min(3, data.length),
+                                  (index) {
+                                    return Container(
+                                      width: 20,
+                                      height: 20,
+                                      margin: EdgeInsets.only(left: index * 14),
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        border: Border.all(color: Colors.white),
+                                        image: DecorationImage(
+                                          image: NetworkImage(
+                                            data[index]["userPhotoURL"],
+                                          ),
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                              const SizedBox(width: 8),
+                              const Text(
+                                "Tampilkan balasan",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.black54,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 14),
+                      ],
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
         );
       },
     );
